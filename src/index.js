31777229eb61d01e0b641f5cfc548f3595c04451
jomiborras <josemanuel.borras@gmail.com>
2022-06-22 15:53:02 -0300

    React-router-dom install and set-up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import Date from './components/Date';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter basename={window.location.pathname || ''}>
     <Date />
-  </React.StrictMode>
+  </BrowserRouter>
 );
