4c71aa1abcd35d6b4fbe4cd31799924efd922a70
jomiborras <josemanuel.borras@gmail.com>
2022-06-22 16:16:17 -0300

    Browser config

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={window.location.pathname || ''}>
+  <BrowserRouter  basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
 );
